af2734cf4903b2699285c135587f2610ca9e6a77
Taejun11 <riee9142@naver.com>
2025-03-06 18:18:10 +0900

    0306a

---
+++ 0306/js/vmenu1_mo.js
@@ -0,0 +1,18 @@
+$(function(){
+    // $('.sub').css('display', 'none');
+    $('.sub').hide();
+    // $('.sub').eq(0).show();
+
+    $('.m_menu > li > a').click(function(e){
+        e.preventDefault();
+        var kkk = $(this).next('.sub').css('display');
+        // 클릭 다음에 있는 요소의 display 상태를 반환함
+        // alert(kkk);
+        if(kkk == 'none'){
+            $('.sub').slideUp();
+            $(this).next('.sub').slideDown();
+        }else{
+            $('.sub').slideUp();
+        }
+    });
+});
