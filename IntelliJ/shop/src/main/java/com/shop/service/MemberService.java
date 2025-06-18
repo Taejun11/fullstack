9fe0180eb3cec746a09610d8ed4012d5e1332f94
Taejun11 <riee9142@naver.com>
2025-06-18 18:13:08 +0900

    0618a

---
+++ IntelliJ/shop/src/main/java/com/shop/service/MemberService.java
@@ -0,0 +1,45 @@
+package com.shop.service;
+
+import com.shop.entity.Member;
+import com.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member){
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if (findMember != null){
+            throw new IllegalArgumentException("이미 가입된 회원입니다.");
+        }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email);
+
+        if(member == null){
+            throw new UsernameNotFoundException(email);
+        }
+//        사용자 정보를 조회, 스프링 시큐리티가 이해할 수 있는 UserDetails로 변환
+        return User.builder()
+                .username(member.getEmail()) //로그인으로 사용할 필드
+                .password(member.getPassword()) //db에 암호화된 비밀번호
+                .roles(member.getRole().toString()) //Role_ADMIN 같은 접두사 자동 등록
+                .build();
+    }
+}
