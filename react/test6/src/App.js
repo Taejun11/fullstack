13d9f706b710bc5ce857acc016c85a3cc0567d6f
Taejun11 <riee9142@naver.com>
2025-07-03 13:17:39 +0900

    0703a

--- react/test6/src/App.js
+++ react/test6/src/App.js
@@ -1,33 +1,78 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 export default function App() {
-    const users = [
+    const [inputs, setInputs] = useState({
+        username: '', email: ''
+    });
+    const { username, email } = inputs; //비구조화 할당(구조분해할당)
+
+    const onChange = e => {
+        const { name, value } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    };
+    const onToggle = id => {
+        setUsers(
+            users.map(user => 
+                user.id === id ? {...user, active : !user.active} : user
+            )
+        );
+    };
+
+    const [users, setUsers] = useState([
         {
             id: 1,
             username: 'velopert',
-            email: 'public.velopert@gmail.com'
+            email: 'public.velopert@gmail.com',
+            active : true
         },
         {
             id: 2,
             username: 'tester',
-            email: 'tester@example.com'
+            email: 'tester@example.com',
+            active : false
         },
         {
             id: 3,
             username: 'liz',
-            email: 'liz@example.com'
+            email: 'liz@example.com',
+            active : false
         }
-    ];
+    ]);
+
     const nextId = useRef(4);
     const onCreate = () => {
-        // 구현할 것
-        nextId.current =+ 1;
+        //구현
+        const user = {
+            id: nextId.current,
+            username,
+            email
+        };
+        setUsers([...users, user]);
+        // setUsers(users.concat(user));
+
+        setInputs({username: '', email: ''
+        });
+        nextId.current = + 1;
+    }
+
+    const onRemove = id => {
+        setUsers(users.filter(user => user.id !== id));
     };
 
     return (
         <div>
-            <UserList users={users} />
+            <CreateUser
+                username={username}
+                email={email}
+                onChange={onChange}
+                onCreate={onCreate}
+            />
+            <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
         </div>
     )
 }
--- react/test6/src/UserList.js
+++ react/test6/src/UserList.js
@@ -1,21 +1,29 @@
 import React from 'react'
 
-function User({ user }) {
+function User({user, onRemove, onToggle}) {
 	return (
 		<div>
-			<b>{user.username}</b> <span>({user.email})</span>
+			<b style={{
+				cursor: "pointer", 
+				color: user.active ? 'green' : 'black'
+			}}
+			onClick={() => onToggle(user.id)}
+			>{user.username}</b> <span>({user.email})</span>
+			<button onClick={() => onRemove(user.id)}>삭제</button>
 		</div>
+		// user 하나의 정보 출력
+		// 삭제 버튼 클릭시 onRemove 실행
+		// user id를 전달하여 어떤 사용자를 삭제할지 부모에게 알려줌
 	);
 
 }
 
-
-export default function UserList({ users }) {
+export default function UserList({users, onRemove, onToggle}) {
 
 	return (
 		<div>
 			{users.map(user => (
-				<User user={user} key={user.id} />
+				<User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>
 			))}
 		</div>
 	)
